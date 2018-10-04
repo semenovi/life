77970a82052ddfd7e94574275314b592e4ec6d0a
semenovi <t79601712352@gmail.com>
2018-10-04 03:11:17 +0300

    check gitignore work

--- include/container.h
+++ include/container.h
@@ -1,8 +1,9 @@
 #ifndef AREA_H
 #define AREA_H
+
 #include <stdio.h>
 #include <algorithm>
-class area
+class container
 {
 private:
 	int ** sheet, ** next_sheet;
@@ -11,13 +12,13 @@ private:
 	int ** allocate_matrix_int(int _w, int _h);
 	void deallocate_matrix_int(int ** _memory, int _w, int _h);
 public:
-	area(int _w, int _h);
+	container(int _w, int _h);
 	void change_cell_state(int _x, int _y, int _state);
 	int get_cell_state(int _x, int _y);
 	int get_width() { return w; };
 	int get_height() { return h; };
 	void apply_next_generation();
-	~area() { 
+	~container() { 
 		deallocate_matrix_int(sheet, w, h);
 		deallocate_matrix_int(next_sheet, w, h);
 	};
--- include/render.h
+++ include/render.h
@@ -5,55 +5,26 @@
 
 #pragma comment(lib, "Opengl32.lib")
 #include <gl/GL.h>
+
 #include <iostream>
 #include <conio.h>
 #include <Windows.h>
 class using_opengl
 {
 private:
-	int left_button_state,
-		right_button_state,
-		x_last_cell,
-		y_last_cell,
-		cell_size,
-		window_width,
-		window_height,
-		Angle;
 	area * area_link;
-	TCHAR *szWndClass;
-	HWND hWnd;
-	HDC hDC;
-	HGLRC hRC;
-	void r(int w, int h);
-	void d();
-	void left(int _x, int _y);
-	void right(int _x, int _y);
-	void m(int button, int state, int x, int y);
-	void mm(int x, int y);
-	void k(unsigned char key, int x, int y);
 	void draw_cells();
 	void draw_grid();
 public:
-	drawer(area * _area_link) :area_link(_area_link)
-	{
-		left_button_state = 0;
-		right_button_state = 0;
-		x_last_cell = -1;
-		y_last_cell = -1;
-		cell_size = 12;
-		window_width = 600;
-		window_height = 300;
-	};
-	void start(int &argc, char * argv[]);
+	using_opengl(area * _area_link) :area_link(_area_link)
+	{};
 };
 class to_command_line
 {
 private:
 	area * area_link;
-	int input_reaction(int _in);
 	void draw_cells(int _cursor);
 public:
 	printer(area * _area_link) :area_link(_area_link) {};
-	void start();
 };
 #endif
